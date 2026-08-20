77cf616b82ca7fcfc7d9ec56eb5fb559d13b9316
Adam Lear <adamlear12@gmail.com>
2026-08-19 18:53:35 -0700

    feat: add gorm

---
+++ internal/db/db.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type DBConfig struct {
+	Host     string
+	User     string
+	Password string
+	DBName   string
+	Port     int
+}
+
+type DB struct {
+	Client *gorm.DB
+}
+
+func New(cfg DBConfig) (*DB, error) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d", cfg.Host, cfg.User, cfg.Password, cfg.DBName, cfg.Port)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return &DB{}, err
+	}
+	return &DB{Client: db}, nil
+}
